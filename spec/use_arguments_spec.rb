9303ee0baffb4fb8d7650a80ae7d30be80dab6bc
manga_osyo <manga.osyo@gmail.com>
2016-02-20 14:55:59 +0900

    Improve rspec.

--- spec/use_arguments_spec.rb
+++ spec/use_arguments_spec.rb
@@ -11,15 +11,19 @@ describe UseArguments do
 		it "Use _1" do
 			expect(proc { _1 + _2 }.use_args.call 1, 2).to eq 3
 		end
-# 		it "Use _" do
-# 			expect(proc { _ + _ }.use_args.call 1).to eq 2
-# 		end
+		it "Use _" do
+			expect(proc { _ + _ }.use_args.call 1).to eq 2
+		end
 		it "Use _args" do
 			expect(proc { _args }.use_args.call 1, 2, 3).to eq [1, 2, 3]
 		end
-		it "Use self" do
-			f = proc { self_ }.use_args
+		it "Use _self" do
+			f = proc { _self }.use_args
 			expect( f.call == f ).to eq true
+		end
+		it "Use _block" do
+			f = proc { _yield 1, 2 }.use_args
+			expect( (f.call do |a, b| a + b end) ).to eq 3
 		end
 	end
 end
